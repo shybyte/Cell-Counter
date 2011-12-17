6169ba17889300146fc54e45083bd3012eb79a93
shybyte <ein@volloeko.de>
2011-12-17 19:01:36 +0100

    more

--- js/cell-counter.coffee
+++ js/cell-counter.coffee
@@ -3,6 +3,20 @@ initCellCounter = () ->
   ctx = canvas.getContext('2d')
 
   init = ->
+    canvas.ondragover = ->
+        this.className = 'ondragover'
+        return false
+
+    canvas.ondragleave = ->
+        this.className = ''
+        return false
+
+    canvas.ondrop = (e) ->
+      this.className = ''
+      e.preventDefault()
+      loadLocalImage(e.dataTransfer.files[0])
+
+
     loadImage('images/nora1.jpg')
 
   loadImage = (src) ->
@@ -10,16 +24,23 @@ initCellCounter = () ->
     img.onload = ->
       canvas.width = img.width
       canvas.height = img.height
-      ctx.drawImage(img, 0, 0);
+      ctx.drawImage(img, 0, 0)
     img.src = src;
+
+  loadLocalImage = (file) ->
+    reader = new FileReader();
+    reader.onload = (event) ->
+        loadImage(event.target.result)
+    reader.readAsDataURL(file)
 
   init()
 
 
 
-
-
-
+checkAPIsAvailable = ->
+  if( typeof window.FileReader == 'undefined')
+    alert("No local file reading possible. Please use a newer version of firefox or google chrome")
 
 jq ->
+  checkAPIsAvailable()
   initCellCounter()
--- js/cell-counter.js
+++ js/cell-counter.js
@@ -1,10 +1,23 @@
 (function() {
-  var initCellCounter;
+  var checkAPIsAvailable, initCellCounter;
   initCellCounter = function() {
-    var canvas, ctx, init, loadImage;
+    var canvas, ctx, init, loadImage, loadLocalImage;
     canvas = $('mainCanvas');
     ctx = canvas.getContext('2d');
     init = function() {
+      canvas.ondragover = function() {
+        this.className = 'ondragover';
+        return false;
+      };
+      canvas.ondragleave = function() {
+        this.className = '';
+        return false;
+      };
+      canvas.ondrop = function(e) {
+        this.className = '';
+        e.preventDefault();
+        return loadLocalImage(e.dataTransfer.files[0]);
+      };
       return loadImage('images/nora1.jpg');
     };
     loadImage = function(src) {
@@ -17,9 +30,23 @@
       };
       return img.src = src;
     };
+    loadLocalImage = function(file) {
+      var reader;
+      reader = new FileReader();
+      reader.onload = function(event) {
+        return loadImage(event.target.result);
+      };
+      return reader.readAsDataURL(file);
+    };
     return init();
   };
+  checkAPIsAvailable = function() {
+    if (typeof window.FileReader === 'undefined') {
+      return alert("No local file reading possible. Please use a newer version of firefox or google chrome");
+    }
+  };
   jq(function() {
+    checkAPIsAvailable();
     return initCellCounter();
   });
 }).call(this);
