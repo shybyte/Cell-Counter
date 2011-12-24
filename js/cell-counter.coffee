7ae34538dcd21bcec4dfc8d3b9c103fe6c7e9cb0
shybyte <ein@volloeko.de>
2011-12-24 23:55:29 +0100

    manual counting works

--- js/cell-counter.coffee
+++ js/cell-counter.coffee
@@ -1,40 +1,83 @@
+markingsIdCounter = 0
+class Marking
+  constructor:(@x, @y) ->
+    @id = markingsIdCounter++
+    pos = {left:@x - 4, top:@y - 4}
+    @el = jq('<div/>').addClass('marking').css(pos).attr('id',@id)
+
+
 initCellCounter = () ->
   canvas = $('mainCanvas')
   ctx = canvas.getContext('2d')
+  markings = []
+  $markings = jq('#markings')
 
   init = ->
+    initDragAndDrop()
+    initManualCounter()
+    loadImage('images/nora1.jpg')
+
+  initDragAndDrop = ->
     canvas.ondragover = ->
-        this.className = 'ondragover'
-        return false
+    this.className = 'ondragover'
+    return false
 
     canvas.ondragleave = ->
-        this.className = ''
-        return false
+      this.className = ''
+      return false
 
     canvas.ondrop = (e) ->
       this.className = ''
       e.preventDefault()
       loadLocalImage(e.dataTransfer.files[0])
 
+  initManualCounter = ->
+    $markings.click((e) ->
+        if e.ctrlKey and markings.length>0
+          removeMarking(e.offsetX, e.offsetY)
+        else
+          addMarking(e.offsetX, e.offsetY)
+    )
 
-    loadImage('images/nora1.jpg')
+  addMarking = (x, y) ->
+    marking = new Marking(x, y)
+    markings.push(marking)
+    $markings.append(marking.el)
+    showCellCount()
+
+  removeMarking = (x, y) ->
+    marking = findNearestMarking(x, y)
+    markings = _.without(markings,marking)
+    marking.el.remove()
+    showCellCount()
+
+  showCellCount = ->
+    jq('#cellCount').text(markings.length)
+
+  findNearestMarking = (x, y) ->
+    _.min(markings, (marking) ->
+      dx = marking.x-x
+      dy = marking.y-y
+      dx*dx+dy*dy
+    )
 
   loadImage = (src) ->
-    img = new Image();
+    img = new Image()
+
     img.onload = ->
       canvas.width = img.width
       canvas.height = img.height
       ctx.drawImage(img, 0, 0)
-    img.src = src;
+    img.src = src
 
   loadLocalImage = (file) ->
-    reader = new FileReader();
+    reader = new FileReader()
     reader.onload = (event) ->
-        loadImage(event.target.result)
+      loadImage(event.target.result)
     reader.readAsDataURL(file)
 
-  init()
 
+  init()
 
 
 checkAPIsAvailable = ->
--- js/cell-counter.js
+++ js/cell-counter.js
@@ -1,24 +1,78 @@
 (function() {
-  var checkAPIsAvailable, initCellCounter;
+  var Marking, checkAPIsAvailable, initCellCounter, markingsIdCounter;
+  markingsIdCounter = 0;
+  Marking = (function() {
+    function Marking(x, y) {
+      var pos;
+      this.x = x;
+      this.y = y;
+      this.id = markingsIdCounter++;
+      pos = {
+        left: this.x - 4,
+        top: this.y - 4
+      };
+      this.el = jq('<div/>').addClass('marking').css(pos).attr('id', this.id);
+    }
+    return Marking;
+  })();
   initCellCounter = function() {
-    var canvas, ctx, init, loadImage, loadLocalImage;
+    var $markings, addMarking, canvas, ctx, findNearestMarking, init, initDragAndDrop, initManualCounter, loadImage, loadLocalImage, markings, removeMarking, showCellCount;
     canvas = $('mainCanvas');
     ctx = canvas.getContext('2d');
+    markings = [];
+    $markings = jq('#markings');
     init = function() {
-      canvas.ondragover = function() {
-        this.className = 'ondragover';
-        return false;
-      };
+      initDragAndDrop();
+      initManualCounter();
+      return loadImage('images/nora1.jpg');
+    };
+    initDragAndDrop = function() {
+      canvas.ondragover = function() {};
+      this.className = 'ondragover';
+      return false;
       canvas.ondragleave = function() {
         this.className = '';
         return false;
       };
-      canvas.ondrop = function(e) {
+      return canvas.ondrop = function(e) {
         this.className = '';
         e.preventDefault();
         return loadLocalImage(e.dataTransfer.files[0]);
       };
-      return loadImage('images/nora1.jpg');
+    };
+    initManualCounter = function() {
+      return $markings.click(function(e) {
+        if (e.ctrlKey && markings.length > 0) {
+          return removeMarking(e.offsetX, e.offsetY);
+        } else {
+          return addMarking(e.offsetX, e.offsetY);
+        }
+      });
+    };
+    addMarking = function(x, y) {
+      var marking;
+      marking = new Marking(x, y);
+      markings.push(marking);
+      $markings.append(marking.el);
+      return showCellCount();
+    };
+    removeMarking = function(x, y) {
+      var marking;
+      marking = findNearestMarking(x, y);
+      markings = _.without(markings, marking);
+      marking.el.remove();
+      return showCellCount();
+    };
+    showCellCount = function() {
+      return jq('#cellCount').text(markings.length);
+    };
+    findNearestMarking = function(x, y) {
+      return _.min(markings, function(marking) {
+        var dx, dy;
+        dx = marking.x - x;
+        dy = marking.y - y;
+        return dx * dx + dy * dy;
+      });
     };
     loadImage = function(src) {
       var img;
