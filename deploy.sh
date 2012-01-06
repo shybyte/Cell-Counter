c94754466c199c5c2919483a09534f2708fa0424
shybyte <ein@volloeko.de>
2012-01-06 16:23:08 +0100

    added minimized deploy version

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,7 @@ cp -r images $TARGET
 cp -r js $TARGET
 cp -r libs $TARGET
 cp -r index.html $TARGET
+echo "Compress javascripts..."
+java -jar buildtools/closure/compiler.jar libs/common.js js/utils.js js/filters.js js/filters-fast.js js/cell-counter.js --js_output_file $TARGET/js/cell-counter.min.js
+echo "Combine javascripts..."
+cat libs/jquery.min.js libs/jquery.tools.min.js libs/underscore-min.js $TARGET/js/cell-counter.min.js >$TARGET/js/cell-counter-with-libs.min.js
