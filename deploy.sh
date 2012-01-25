e4d839c3965ff21acc797d3fb9b0f1987f882dfe
shybyte <ein@volloeko.de>
2012-01-25 09:53:17 +0100

    Added update message

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,6 @@ cp index-dev.html $TARGET
 cp manifest.appcache $TARGET
 echo "Compress javascripts..."
 java -jar buildtools/closure/compiler.jar libs/common.js js/utils.js js/filters.js js/filters-fast.js js/cell-counter.js --js_output_file $TARGET/js/cell-counter.min.js
+#cat libs/common.js js/utils.js js/filters.js js/filters-fast.js js/cell-counter.js >$TARGET/js/cell-counter.min.js
 echo "Combine javascripts..."
 cat libs/jquery.min.js libs/jquery.tools.min.js libs/underscore-min.js $TARGET/js/cell-counter.min.js >$TARGET/js/cell-counter-with-libs.min.js
